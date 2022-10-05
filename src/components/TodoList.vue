4c02bf8dc766ba6be53ced55e7fd34bbda13b9d6
Dyoung <wkftkfwk0118@gmail.com>
2022-10-05 11:47:36 +0900

    slot변수전달

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,33 +1,38 @@
 <template>
   <div>
-    <div v-for="(item, index) in todos" v-bind:key="index" class="card mt-2">
-      <div class="card-body p-2 d-flex">
-        <div class="form-check flex-grow-1 align-items-center">
-          <input
-            type="checkbox"
-            class="form-check-input"
-            :checked="item.complete"
-            @change="toggleTodo(index)"
-          />
-          <label
-            @click="moveToPage(item.id)"
-            style="cursor: pointer"
-            class="form-check-label"
-            v-bind:class="{ todostyle: item.complete }"
-            >{{ item.subject }}
-          </label>
+    <!-- <div v-for="(item, index) in todos" v-bind:key="index" class="card mt-2"> -->
+    <ListView :items="todos">
+      <template #default="{ item, index }">
+        <div class="card-body p-2 d-flex">
+          <div class="form-check flex-grow-1 align-items-center">
+            <input
+              type="checkbox"
+              class="form-check-input"
+              :checked="item.complete"
+              @change="toggleTodo(index)"
+            />
+            <label
+              @click="moveToPage(item.id)"
+              style="cursor: pointer"
+              class="form-check-label"
+              v-bind:class="{ todostyle: item.complete }"
+              >{{ item.subject }}
+            </label>
+          </div>
+          <button class="btn btn-danger btn-sm" @click="openModal(item.id)">
+            Delete
+          </button>
         </div>
-        <button class="btn btn-danger btn-sm" @click="openModal(item.id)">
-          Delete
-        </button>
-      </div>
-    </div>
+      </template>
+    </ListView>
+    <!-- </div> -->
 
     <teleport to="#modal">
       <DeleteModal
         v-if="showModal"
         @close-modal="closeModal"
         @delete="onDelete"
+        @close="closeModal"
       />
     </teleport>
   </div>
@@ -37,9 +42,11 @@
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import DeleteModal from "@/components/DeleteModal.vue";
+import ListView from "@/components/ListView.vue";
 export default {
   components: {
     DeleteModal,
+    ListView,
   },
   props: ["todos"],
   emits: ["delete-todo", "toggle-todo"],
